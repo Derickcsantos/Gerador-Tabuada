8b390c756498b3de86a5a53b2457386d03b00fa2
Dérick Campos Santos <164596759+Derickcsantos@users.noreply.github.com>
2025-07-23 16:31:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const calcularBtn = document.getElementById("calculoBtn");
+
+function calcularTabuada(){
+    const valor = document.getElementById('inputNumero').value;
+    let texto = document.getElementById('texto');
+    let recebeResultado = document.getElementById('recebeResultado');
+    
+    for (let i = 0; i < 10; i++){
+        const valorFinal = document.createElement('p');
+        valorFinal.textContent = `${valor} x ${i} = ${valor*i}`
+
+        console.log(valorFinal);
+
+        recebeResultado.appendChild(valorFinal);
+
+    }
+
+    const mensagemResultado = document.getElementById('mensagemResultado').innerText = `Abaixo está a tabuada do número ${valor}. `
+    document.getElementById('inputNumero').value = '';
+    return console.log('Funcionou!')
+}
+
+calcularBtn.addEventListener('click', () =>{
+    calcularTabuada()
+})
